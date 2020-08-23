6f927807b59a770b14252f24a46d310b622b706a
Maiker Gonzales <maiker.gonzales@vallegrande.edu.pe>
2020-08-22 22:52:30 -0500

    Analizando rostro con Servicio Cognitivo - Face

---
+++ SCface/src/main/java/modelo/Atributos.java
@@ -0,0 +1,152 @@
+package modelo;
+
+public class Atributos {
+
+    private String url;
+    private String hair;
+    private String hairColor;
+    private String smile;
+    private String headPose;
+    private String gender;
+    private String age;
+    private String facialHair;
+    private String glasses;
+    private String makeup;
+    private String emotion;
+    private String occlusion;
+    private String accessories;
+    private String blur;
+    private String exposure;
+    private String noise;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getSmile() {
+        return smile;
+    }
+
+    public void setSmile(String smile) {
+        this.smile = smile;
+    }
+
+    public String getHeadPose() {
+        return headPose;
+    }
+
+    public void setHeadPose(String headPose) {
+        this.headPose = headPose;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getFacialHair() {
+        return facialHair;
+    }
+
+    public void setFacialHair(String facialHair) {
+        this.facialHair = facialHair;
+    }
+
+    public String getGlasses() {
+        return glasses;
+    }
+
+    public void setGlasses(String glasses) {
+        this.glasses = glasses;
+    }
+
+    public String getMakeup() {
+        return makeup;
+    }
+
+    public void setMakeup(String makeup) {
+        this.makeup = makeup;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public String getOcclusion() {
+        return occlusion;
+    }
+
+    public void setOcclusion(String occlusion) {
+        this.occlusion = occlusion;
+    }
+
+    public String getAccessories() {
+        return accessories;
+    }
+
+    public void setAccessories(String accessories) {
+        this.accessories = accessories;
+    }
+
+    public String getBlur() {
+        return blur;
+    }
+
+    public void setBlur(String blur) {
+        this.blur = blur;
+    }
+
+    public String getExposure() {
+        return exposure;
+    }
+
+    public void setExposure(String exposure) {
+        this.exposure = exposure;
+    }
+
+    public String getNoise() {
+        return noise;
+    }
+
+    public void setNoise(String noise) {
+        this.noise = noise;
+    }
+    
+    
+
+}
